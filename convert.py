9da6f0ae58fe81f5c2a0c76852cc1d9a226eb902
Ajibola Ojo <41059344+jbrit@users.noreply.github.com>
2021-05-02 23:44:13 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -5,6 +5,8 @@ class CidrMaskConvert:
     def cidr_to_mask(self, val):
         values = []
         val = int(val)
+        if 0 >= val or val > 32:
+            return "Invalid"
         for _ in range(4):
             values.append(val if val <= 8 else 8)
             val = val-val if val <=8 else val-8
@@ -13,13 +15,16 @@ class CidrMaskConvert:
     def mask_to_cidr(self, val):
         values = val.split(".")
         values = list(map(lambda x: 8 - math.log2(2**8 - int(x)), values))
-        return str(int(sum(values)))
+        total = int(sum(values))
+        if len(values) != 4 or any(int(num) > 255 or int(num) < 0 for num in values) or total == 0:
+            return "Invalid"
+        return str(total)
 
 class IpValidate:
 
     def ipv4_validation(self, val):
         values = val.split('.')
-        if not len(values) == 4:
+        if not len(values) == 4 or any(char.isalpha() for char in val):
             return False
         for value in values:
             if not 0 <= int(value) <= 255:
